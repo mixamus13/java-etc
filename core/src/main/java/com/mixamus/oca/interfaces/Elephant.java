c852d4f0bbe03c9fd6241bfe4735ccd6dbf700df
Maxim Dokuchaev <m.dokuchaev@intellect.group>
2022-05-10 09:07:06 +0300

    add oca module interfaces

--- core/src/main/java/com/mixamus/oca/interfaces/Elephant.java
+++ core/src/main/java/com/mixamus/oca/interfaces/Elephant.java
@@ -1,7 +1,7 @@
 package com.mixamus.oca.interfaces;
 
 public class Elephant extends Fly
-                      implements WalksOnFourLegs, HasTrunk, Herbivore {
+        implements WalksOnFourLegs, HasTrunk, Herbivore {
     @Override
     public void doSome() {
         System.out.println("do");
@@ -9,16 +9,29 @@ public class Elephant extends Fly
 
     public static void main(String... args) {
         WalksOnFourLegs legs = new Elephant();
-        System.out.println("WalksOnFourLegs.MINIMUM = " + WalksOnFourLegs.MINIMUM);
+        System.out.println("WalksOnFourLegs.MINIMUM = " + WalksOnFourLegs.MINIMUM_DEPTH);
+    }
+
+    @Override
+    public void doDefault() {
+        WalksOnFourLegs.super.doDefault();
     }
 }
 
 
 // интерфейс это контракт, API, который должен реализовать класс
 abstract interface WalksOnFourLegs {
-    public static final int MINIMUM = 2;
+    public static final int MINIMUM_DEPTH = 13;
+    public static final String TYPE = "Setting";
 
     public abstract void doSome();
+
+    /**
+     * Это костыль
+     */
+    public default void doDefault() {
+        System.out.println();
+    }
 }
 
 
