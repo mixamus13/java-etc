43138eb2d2af559255e9de02f2ca41b91bbef852
Maxim Dokuchaev <m.dokuchaev@intellect.group>
2022-05-18 18:47:56 +0300

    add java problem 105

---
+++ java-problems-book/src/main/java/org/example/javaproblem/p104_reverce_array/Main.java
@@ -0,0 +1,25 @@
+package org.example.javaproblem.p104_reverce_array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String... args) {
+        int[] integers = {-1, 2, 3, 1, 4, 5, 3, 2, 22};
+        Melon[] melons = {new Melon("Crenshaw", 2000), new Melon("Gac", 3200), new Melon("Bitter", 5200)};
+
+        IntStream intStream = IntStream.rangeClosed(1, integers.length)
+                .map(operand -> integers[integers.length - operand]);
+        intStream.forEach(System.out::println);
+
+        System.out.println("before melons = " + Arrays.toString(melons));
+        //Collections.reverse(Arrays.asList(melons));
+        System.out.println("after melons = " + Arrays.toString(melons));
+
+        Melon[] melonsImmutable = IntStream.rangeClosed(1, melons.length)
+                .mapToObj(operand -> melons[melons.length - operand])
+                .toArray(Melon[]::new);
+        System.out.println("melonsImmutable = " + Arrays.toString(melonsImmutable));
+    }
+}
---
+++ java-problems-book/src/main/java/org/example/javaproblem/p104_reverce_array/Melon.java
@@ -0,0 +1,9 @@
+package org.example.javaproblem.p104_reverce_array;
+
+import lombok.Value;
+
+@Value
+public class Melon {
+    String type;
+    int weight;
+}
